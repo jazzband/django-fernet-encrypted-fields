c7782281dd78cabcd337901d500fd2ff8e7ed8a8
naohide <57.x.mas@gmail.com>
2021-10-01 08:04:22 +0900

    Fix setup.py

--- setup.py
+++ setup.py
@@ -6,12 +6,14 @@ setup(
     description=(
         'This is inspired by django-encrypted-fields.'
     ),
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
     url='http://github.com/frgmt/django-fernet-encrypted-fields/',
     license='MIT',
     author='fragment.co.jp',
     author_email='info@fragment.co.jp',
     packages=['encrypted_fields'],
-    version='0.0.1',
+    version='0.0.2',
     install_requires=[
         'Django>=2.2',
         'cryptography>=35.0.0',

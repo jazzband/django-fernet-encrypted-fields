04625a8e406c63aed83cedfc4f9df38b53a277da
naohide <57.x.mas@gmail.com>
2021-12-19 13:19:32 +0900

    Fix setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     author='fragment.co.jp',
     author_email='info@fragment.co.jp',
     packages=['encrypted_fields'],
-    version='0.1.0',
+    version='0.1.1',
     install_requires=[
         'Django>=2.2',
         'cryptography>=35.0.0',
